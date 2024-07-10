36a2b33651303f26d5f324df407a6de423a4b071
dc-hub0 <deepakchauhan7721@gmail.com>
2024-07-10 19:29:03 +0530

    Adding Database

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './db/connectDB.js';
+
+dotenv.config();
+
+connectDB();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server Started at http://localhost:${PORT}`))
