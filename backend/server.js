412fe207817f342879f2c6db356dddd3d8e9503b
dc-hub0 <deepakchauhan7721@gmail.com>
2024-07-11 20:50:11 +0530

    New Folders,Routes,Tokens,functions

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './db/connectDB.js';
 import cookieParser from 'cookie-parser';
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config();
 
@@ -13,5 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); 
 app.use(cookieParser());
+
+app.use("/api/users",userRoutes)
 
 app.listen(PORT, () => console.log(`Server Started at http://localhost:${PORT}`))
