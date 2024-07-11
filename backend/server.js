6312902259df8c0f02c5640694a64e5d12dd3924
dc-hub0 <deepakchauhan7721@gmail.com>
2024-07-11 19:47:17 +0530

    urienc  and .json added in server

--- backend/server.js
+++ backend/server.js
@@ -9,4 +9,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true })); 
+
+
 app.listen(PORT, () => console.log(`Server Started at http://localhost:${PORT}`))
