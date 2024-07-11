61ac818758f72ea84f38a1f0e0b787eb793a0232
dc-hub0 <deepakchauhan7721@gmail.com>
2024-07-11 19:49:00 +0530

    adding cookie parser form its respective module

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './db/connectDB.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -11,6 +12,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); 
-
+app.use(cookieParser());
 
 app.listen(PORT, () => console.log(`Server Started at http://localhost:${PORT}`))
