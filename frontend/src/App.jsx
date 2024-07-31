38b2ca76c03eb7632296602bfd34ec162a3a86cd
dc-hub0 <deepakchauhan7721@gmail.com>
2024-07-31 19:51:06 +0530

    Adding HomePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Button, Container } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import UserPage from "./pages/UserPage";
 import PostPage from "./pages/PostPage";
+import HomePage from "./pages/HomePage";
 import Header from "./components/Header";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <Container maxW="620px">
       <Header />
       <Routes>
+        <Route path='/' element={<HomePage />} />
       <Route path="/:username" element={<UserPage />}/>
       <Route path="/:username/post/:pid" element={<PostPage />}/>
       </Routes>
